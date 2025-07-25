51da8c2a2a2bc935dad97dfe6339533288556f9c
umar57373 <shaikv0111@gmail.com>
2025-07-25 19:44:30 +0530

    Create max_of_three.py

---
+++ max_of_three.py
@@ -0,0 +1,8 @@
+def max_of_three(a,b,c):
+    arr=[a,b,c]
+    return max(arr)
+a=int(input("Enter the 1st Number: "))
+b=int(input("Enter the 2nd Number: "))
+c=int(input("Enter the 3rd Number: "))
+print(f"maximum number is {max_of_three(a,b,c)}")
+
